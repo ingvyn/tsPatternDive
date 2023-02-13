4306d01998c1deac4a77328f4563d5b47d94a5b6
Igor Nick <ingvyn@yandex.ru>
2023-02-13 13:25:51 +0300

    fix union type writings of toWords argument

--- 2-typings/baseTypes.ts
+++ 2-typings/baseTypes.ts
@@ -28,7 +28,7 @@ const TENTHS_LESS_THAN_HUNDRED: string[] = [
  * @param {boolean} [asOrdinal] - Deprecated, use toWordsOrdinal() instead!
  * @returns {string}
  */
-function toWords(number: (number | string), asOrdinal: boolean): string {
+function toWords(number: number | string, asOrdinal: boolean): string {
     const num = typeof number === 'string' ?
         parseInt(number, 10) :
         number;
